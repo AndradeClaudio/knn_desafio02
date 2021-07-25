6aa0c8aee324fb6060937ebdd13eb7033630449e
Claudio de Andrade <andsil@gmail.com>
2021-07-25 12:06:43 -0300

    Formatacao knn

--- knn.py
+++ knn.py
@@ -1,14 +1,14 @@
 class knn():
     """ Classe para calcula distância entre a referencia e os dados modelo \n """
-    def __init__(self,dados):
+
+    def __init__(self, dados):
         """ Metódo construtor da classe knn \n
         Args:
             dados: Dados modelos para buscar referência\n
         Returns: \n
         """
-    
         self.dados = dados
-        return 
+        return
 
     def calcula_distancia(self, busca):
         """ Calcula distância entre referência e os dados modelo \n
@@ -20,11 +20,12 @@ class knn():
         linha = []
         for j in range(len(self.dados)):
             linha.append((((busca[2][0]-self.dados[j][2][0])**2
-                                + (busca[2][1]-self.dados[j][2][1])**2
-                                + (busca[2][2]-self.dados[j][2][2])**2
-                                + (busca[2][3]-self.dados[j][2][3])**2)**0.5))
+                           + (busca[2][1]-self.dados[j][2][1])**2
+                           + (busca[2][2]-self.dados[j][2][2])**2
+                           + (busca[2][3]-self.dados[j][2][3])**2)**0.5))
         return(linha)
-    def k_vizinhos(self,linha:list,k=5):
+
+    def k_vizinhos(self, linha: list, k=5):
         """Busca os vizinhos de acordo com frequência \n
         Args: \n
             linha: Lista com as distancia calculadas \n
@@ -43,7 +44,7 @@ class knn():
             i += 1
         return(resultado)
 
-    def retorna_perfil(self, lista:list):
+    def retorna_perfil(self, lista: list):
         """Retorna o perfil de acordo com a chave \n
         Args: \n
             lista: Lista com os vizinhos mais próximo \n
@@ -55,7 +56,7 @@ class knn():
             resultado.append(self.dados[lista[i][0]][1])
         return resultado
 
-    def moda_lista(self,lista:list):
+    def moda_lista(self, lista: list):
         """Retorna moda referente a lista enviada \n
         Args: \n
             lista: Lista com os vizinhos mais próximo \n
